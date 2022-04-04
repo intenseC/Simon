246dafc691ab2a0a73dc768bf80922bd04ce06fc
intenseC <51984663+intenseC@users.noreply.github.com>
2022-04-04 17:07:36 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,81 +1,54 @@
 /*
  * Simon Says Game Core App
  * author:
- * Simon 'intenseC' Katznelson
-*/
+ * Simon 'intenceC' Katznelson
+ */
 //=================================================================
 //=================================================================
 
- var styles = require('./SRC/cmn.js');
- import './SRC/cmn.js'
- import './SRC/global.js'
- import mainScreen from './SRC/mainScr.js'
+var styles = require('./src/cmn.js');
+import './src/cmn.js';
+import './src/global.js';
+import mainScreen from './src/mainScr.js';
 
- import
-    {
-    /**/
-    increment,
-    decrement,
-    setVal,
-    setIdx,
-    store,
+import {
+  /**/
+  increment,
+  decrement,
+  setVal,
+  setIdx,
+  store,
+} from './src/eRedux.js';
 
-    }
-    from './SRC/eRedux.js'
+import Tone from 'react-native-tone-android';
 
- import
- Tone
- from "react-native-tone-android";
+import {Provider, connect} from 'react-redux';
 
- import {
-	 Provider,
-	 connect
-        }
-	 from 'react-redux';
+import {applyMiddleware, createStore, combineReducers} from 'redux';
 
- import {
-     applyMiddleware,
-	 createStore,
-	 combineReducers
-		}
-	    from 'redux';
-
- import {
+import {
   createReduxContainer,
   createNavigationReducer,
-        }
-  from 'react-navigation-redux-helpers';
+} from 'react-navigation-redux-helpers';
 
- import {
+import {
   createReduxBoundAddListener,
   createReactNavigationReduxMiddleware,
-        }
-  from 'react-navigation-redux-helpers';
+} from 'react-navigation-redux-helpers';
 
- import {
-     widthPercentageToDP  as wp,
-     heightPercentageToDP as hp
-        }
-  from 'react-native-responsive-screen';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
- import * as React from 'react';
- import {
-	NavigationContainer 
-	} 
-	from '@react-navigation/native';
- import {
-	createNativeStackNavigator 
-        }
-	from '@react-navigation/native-stack';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
- import type
-        {
-	    Node
-        }
-	from 'react';
+import type {Node} from 'react';
 
- import {
-  Button, 
+import {
+  Button,
   SafeAreaView,
   RefreshControl,
   ScrollView,
@@ -85,134 +58,118 @@
   useColorScheme,
   View,
   TouchableOpacity,
-  AppRegistry
-        }
-  from 'react-native';
+  AppRegistry,
+} from 'react-native';
 
- import {
+import {
   Colors,
   DebugInstructions,
   Header,
   LearnMoreLinks,
   ReloadInstructions,
-        }
-  from 'react-native/Libraries/NewAppScreen';
+} from 'react-native/Libraries/NewAppScreen';
 
- import {
-  createStackNavigator,
-        }
-  from 'react-navigation-stack';
+import {createStackNavigator} from 'react-navigation-stack';
 
- import {
-  StackNavigator,
-  addNavigationHelpers,
-        }
-   from 'react-navigation';
+import {StackNavigator, addNavigationHelpers} from 'react-navigation';
 
- import {
-    useState,
-    useEffect,
-    useRef
-        }
-   from 'react';
+import {useState, useEffect, useRef} from 'react';
 
- import
-    AsyncStorage
-    from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 //=================================================================
 //=================================================================
- global.Stack = createNativeStackNavigator();
- global.Score = 0;
+global.Stack = createNativeStackNavigator();
+global.Score = 0;
 
- let cScore;
- let displ;
- let scoreTable = '';
- global.fillScr = 0;
+let cScore;
+let displ;
+let scoreTable = '';
+global.fillScr = 0;
 //=================================================================
 //=================================================================
-{/*
-*/}
+{
+  /*
+   */
+}
 //=================================================================
 //=================================================================
 
-const  storeData = async (key, value) => {
+const storeData = async (key, value) => {
   try {
     await AsyncStorage.setItem(key, value);
-      } catch (e) { console.log(e);
-   }
-}
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-const getData = async (key) => {
-    if(typeof key === 'undefined')  key = 'Key';
+const getData = async key => {
+  if (typeof key === 'undefined') {
+    key = 'Key';
+  }
   try {
     const value = await AsyncStorage.getItem(key);
-     if(value !== null) {
-         return (value);
-      }
-    } catch(e) { console.log(e);
+    if (value !== null) {
+      return value;
+    }
+  } catch (e) {
+    console.log(e);
   }
-}
-
+};
 
 /* */
 //=================================================================
 //=================================================================
 // Build the table
-async function fillScores() {
-    let name    = '';
-    let score   = '';
-    scoreTable  = '';
-    let len     =  0;
+const fillScores = async () => {
+  let name = '';
+  let score = '';
+  scoreTable = '';
+  let len = 0;
 
-         scoreTable += (typeof cScore !== 'undefined') ?
-        `\n  You scored ${cScore} times! \n\n` : '\n\n';
-//   on a fresh install only
-     name = await getData('_name1'); 
-     if(typeof name === 'undefined')
-     for(let y = 1; y < 11; y++) {
-      await storeData('_name' + y.toString(), "???" );
+  scoreTable += cScore ?
+  `\n  You scored ${cScore} times! \n\n` : '\n\n';
+
+  name = await getData('_name1');
+  if (typeof name === 'undefined') {
+    for (let y = 1; y < 11; y++) {
+      await storeData('_name' + y.toString(), '???');
       await storeData('_key' + y.toString(), '0');
-	 }
+    }
+  }
 
-       for(let y = 1; y < 11; y++) {
-       name =  await getData('_name' + y.toString());
-//     do some padding here, note that font is monospaced
-       name = name.substring(0, 10); // limit to 10 chars
-	   len  = (name.length < 10) ? 10 - name.length : 0;
-	   while(len--)  name += '\u00A0';
-/**/
-       scoreTable += 'PLAYER:\u00A0';
-       scoreTable += (typeof name !== 'undefined') ? name : '';
-       scoreTable +=  '\u00A0\u00A0';
-       scoreTable += 'SCORE:\u00A0';
-       score = await getData('_key' + y.toString());
-       scoreTable += (parseInt(score) < 10) ? ' 0' : ' ';
-       scoreTable += (typeof score !== 'undefined') ? score : '';
-       scoreTable += '\n';
-      }
-//      console.log(scoreTable);
+  for (let y = 1; y < 11; y++) {
+    name = await getData('_name' + y.toString());
+    name = name.substring(0, 10);
+    len = name.length < 10 ? 10 - name.length : 0;
+    while (len--) name += '\u00A0';
+
+    score = await getData('_key' + y.toString());
+    scoreTable += `PLAYER:\u00A0 ${ typeof name !== 'undefined' ? name : '' } \u00A0\u00A0SCORE:\u00A0 ${
+      parseInt(score) < 10 ? ' 0' : ' '}${ typeof score !== 'undefined' ? score : ''} \n`;
+  }
+
 }
 //=================================================================
 //=================================================================
 
-export default
-function App() {
-
-    if(typeof store.getState().reducer.value === 'undefined') {
+export default function App() {
+  if (typeof store.getState().reducer.value === 'undefined') {
     global.Score = 0;
     store.dispatch(setVal());
-    }
-    if(typeof store.getState().reducer.idx   === 'undefined') {
+  }
+  if (typeof store.getState().reducer.idx === 'undefined') {
     global.Idx = 0;
     store.dispatch(setIdx());
-    }
+  }
+
+  //fillScores();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName = "Score Table">
-        <Stack.Screen name = "Score Table" component = { scoreScreen } />
-        <Stack.Screen name = "Simon Says" component = { mainScreen } />
-        <Stack.Screen name = "About" component = { aboutScreen } />
+      <Stack.Navigator initialRouteName="Score Table">
+        <Stack.Screen name="Score Table" component={scoreScreen} />
+        <Stack.Screen name="Simon Says" component={mainScreen} />
+        <Stack.Screen name="About" component={aboutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -220,80 +177,88 @@ function App() {
 
 function aboutScreen() {
   return (
-    <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text style = {{ textAlign: "center" }}>
-    { "Simon the Game.\n  Written by Simon Katznelson. \nThis Program is Free and Open Source." }
-    </Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{textAlign: 'center'}}>
+        {
+          'Simon the Game.\n  Written by Simon Katznelson. \nThis Program is Free and Open Source.'
+        }
+      </Text>
     </View>
   );
 }
 
-function scoreScreen({ navigation }) {
- // hooks only, no constructor
-let [ count, setCount ]   = useState(store.getState().reducer.value);
-let [ msg, setMsg ]       = useState('');
-const scrlViewRef = useRef();
+function scoreScreen({navigation}) {
+  // hooks only, no constructor
+  let [count, setCount] = useState(store.getState().reducer.value);
+  let [msg, setMsg] = useState('');
+  const scrlViewRef = useRef();
 
-useEffect( ()  => {
-  fillScores();
-},             []);
+  useEffect(() => {
+    fillScores();
+  }, []);
 
-   /* refreshing / updating states here */
-displ = setInterval( () => {
-	if(global.fillScr == 1) {
+  /* refreshing / updating states here */
+  displ = setInterval(() => {
+    if (global.fillScr == 1) {
       global.fillScr = 0;
       fillScores();
-	}
-   setCount(count = store.getState().reducer.value > 4 ?
-   store.getState().reducer.value - 4 : 0);
-   cScore = count;
-   global.Idx = store.getState().reducer.idx;
-   setMsg(msg = scoreTable);
-  },
-   200)
+    }
+    setCount(
+      (count =
+        store.getState().reducer.value > 4
+          ? store.getState().reducer.value - 4
+          : 0),
+    );
+    cScore = count;
+    global.Idx = store.getState().reducer.idx;
+    setMsg((msg = scoreTable));
+  }, 200);
 
   return (
-    <View   style = { styles.container }>
-    <View>
-    <ScrollView
-    ref =   { scrlViewRef }
-   onContentSizeChange = {(contentWidth, contentHeight) =>
-            { scrlViewRef.current.scrollToEnd({ animated: true })
-        }
-    }
-      style = { styles.scorOutput }>
-        <Text style = { styles.scores }>
-         { msg }
-        </Text>
-      </ScrollView>
+    <View style={styles.container}>
+      <View>
+        <ScrollView
+          ref={scrlViewRef}
+          onContentSizeChange={(contentWidth, contentHeight) => {
+            scrlViewRef.current.scrollToEnd({animated: true});
+          }}
+          style={styles.scorOutput}>
+          <Text style={styles.scores}>
+            {msg /* `You scored ${count} times ` + msg */}
+          </Text>
+        </ScrollView>
 
-      <Text></Text>
-      <Button title   = "Start A New Game"
-        onPress = { () => {
-            clearInterval(displ), navigation.navigate("Simon Says")
-        } }/>
+        <Text />
+        <Button
+          title="Start A New Game"
+          onPress={() => {
+            clearInterval(displ), navigation.navigate('Simon Says');
+          }}
+        />
 
-
-              <Text></Text>
-      <Button title   = "update score"
-        onPress = {  () => {
+        <Text />
+        <Button
+          title="update score"
+          onPress={() => {
             fillScores();
-        } } />
+          }}
+        />
 
-              <Text></Text>
-      <Button title   = "about"
-        onPress = {  () => {
+        <Text />
+        <Button
+          title="about"
+          onPress={() => {
             fillScores();
-            navigation.navigate("About");
-        } }/>
-
+            navigation.navigate('About');
+            //          console.log('idx :' + 0),
+          }}
+        />
+      </View>
     </View>
-    </View>
-
+    /* */
   );
 }
-		/*   */
+/*   */
 //=================================================================
 //=================================================================
-		/*   */
-
+/*   */
